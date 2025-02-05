2243b8879132cd5ed69d0c3e002aefe22a4de38b
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-05 11:54:29 +0530

    .

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Watermark() {
   return (
     <div className="fixed top-4 right-4 text-gray-300 text-sm font-semibold transform rotate-[-45deg] pointer-events-none select-none">
-      Created by Harsh Mriduhash
+      Created by Harsh Mriduhash.
     </div>
   );
 }
