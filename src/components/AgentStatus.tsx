d75b7900b81b05ce4f5ebc71ca4bbd3a6586ffa5
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-02-05 10:17:57 +0530

    Agent

--- src/components/AgentStatus.tsx
+++ src/components/AgentStatus.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { AIAgent } from '../types';
-import { Bot } from 'lucide-react';
+import React from "react";
+import { AIAgent } from "../types";
+import { Bot } from "lucide-react";
 
 interface AgentStatusProps {
   agent: AIAgent;
